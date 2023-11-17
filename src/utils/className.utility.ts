0a995aed7f1b503ecb7423f28417b519dccd5a29
andonirdgz <andoni_rdgz@outlook.com>
2023-11-16 20:49:39 -0600

    :bulb: added function annotation

--- src/utils/className.utility.ts
+++ src/utils/className.utility.ts
@@ -1,6 +1,11 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+/**
+ * A utility function that merges Tailwind classes with clsx.
+ * @param inputs - The Tailwind classes to merge.
+ * @returns The merged Tailwind classes.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
