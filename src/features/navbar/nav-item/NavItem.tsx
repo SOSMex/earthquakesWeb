4bccc88c63ec8c4f8476790f3152fc0368257910
DerekRdgz <derek_rdgz@outlook.com>
2023-12-05 15:47:59 -0600

    refactor: :recycle: Updated the Download Button

---
+++ src/features/navbar/nav-item/NavItem.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import { NavItemProps } from './model';
+import { cn } from '@/utils';
+
+export function NavItem(props: NavItemProps) {
+  const { className, href, label } = props;
+
+  return (
+    <Link
+      className={cn(
+        'my-5 cursor-pointer font-bold hover:text-[#48347A] hover:underline hover:decoration-[#72629B] hover:decoration-4 hover:underline-offset-[0.75rem] md:mx-4 md:my-0',
+        className,
+      )}
+      href={href}
+    >
+      {label}
+    </Link>
+  );
+}
