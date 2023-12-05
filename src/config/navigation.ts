3b98213efd62af070acef0068d350c3e182f9a4d
DerekRdgz <derek_rdgz@outlook.com>
2023-12-05 15:34:03 -0600

    refactor: :art: Updated the format of Navigation Item Props

---
+++ src/config/navigation.ts
@@ -0,0 +1,20 @@
+import { NavItemProps } from '@/features';
+
+export const navigation: NavItemProps[] = [
+  {
+    label: 'Inicio',
+    href: '/',
+  },
+  {
+    label: 'Listado de sismos',
+    href: '/earthquakes',
+  },
+  {
+    label: 'Acerca de',
+    href: '/about',
+  },
+  {
+    label: 'Legal',
+    href: '/legal',
+  },
+];
