abcd471da030b0c40a931aeb89ac95f02b0e3ac8
DerekRdgz <derek_rdgz@outlook.com>
2023-12-07 15:13:51 -0600

    refactor: :recycle: Added onClick prop

--- src/features/navbar/nav-item/NavItem.tsx
+++ src/features/navbar/nav-item/NavItem.tsx
@@ -3,10 +3,11 @@ import { NavItemProps } from './model';
 import { cn } from '@/utils';
 
 export function NavItem(props: NavItemProps) {
-  const { className, href, label } = props;
+  const { className, href, label, onClick } = props;
 
   return (
     <Link
+      onClick={onClick}
       className={cn(
         'my-5 cursor-pointer font-bold hover:text-[#48347A] hover:underline hover:decoration-[#72629B] hover:decoration-4 hover:underline-offset-[0.75rem] md:mx-4 md:my-0',
         className,
--- src/features/navbar/nav-item/model.ts
+++ src/features/navbar/nav-item/model.ts
@@ -2,4 +2,5 @@ export interface NavItemProps {
   label: string;
   href: string;
   className?: string;
+  onClick?: () => void;
 }
