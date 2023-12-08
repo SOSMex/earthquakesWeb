660c97c8efa1120c6cd7871b8296445854b84050
andonirdgz <andoni_rdgz@outlook.com>
2023-12-08 14:01:28 -0600

    refactor: :wheelchair: semantics and structure improvements

--- src/features/navbar/desktop/NavbarDesktop.tsx
+++ src/features/navbar/desktop/NavbarDesktop.tsx
@@ -1,12 +1,18 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
 import { NavItem } from '../nav-item/NavItem';
 import { NavbarDesktopProps } from './model';
+import { AppDownloadButton } from '@/components/widgets';
 
 export function NavbarDesktop(props: NavbarDesktopProps) {
   const { navigation } = props;
+  const pathname = usePathname();
+
   return (
-    <nav className="container sticky top-0 mx-auto flex items-center justify-between bg-primary px-8 py-4 max-md:hidden">
+    <header className="container sticky top-0 mx-auto flex items-center justify-between bg-background px-8 py-4 max-md:hidden">
       <Link href="/">
         <Image
           alt="Sismos México"
@@ -16,18 +22,14 @@ export function NavbarDesktop(props: NavbarDesktopProps) {
           height={57}
         />
       </Link>
-
-      <div className="flex bg-primary transition-all duration-500 ease-in ">
-        {navigation.map((item) => (
-          <NavItem {...item} />
-        ))}
-      </div>
-      <button
-        type="button"
-        className="rounded-3xl bg-[#48347A] px-4 py-2 font-bold text-white hover:bg-purple-600 hover:duration-200"
-      >
-        Descarga la app
-      </button>
-    </nav>
+      <nav>
+        <ul className="flex gap-10 transition-all duration-500 ease-in">
+          {navigation.map((item) => (
+            <NavItem {...item} active={pathname === item.href} />
+          ))}
+        </ul>
+      </nav>
+      <AppDownloadButton />
+    </header>
   );
 }
--- src/features/navbar/mobile/NavbarMobile.tsx
+++ src/features/navbar/mobile/NavbarMobile.tsx
@@ -1,16 +1,19 @@
 'use client';
 
+import { usePathname } from 'next/navigation';
 import { useEffect, useRef, useState } from 'react';
 import { Menu } from 'lucide-react';
 import { NavbarMobileProps } from './model';
 import { NavItem } from '@/features';
 import { cn } from '@/lib/utils';
+import { AppDownloadButton } from '@/components/widgets';
 
 export function NavbarMobile(props: NavbarMobileProps) {
   const { navigation } = props;
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const menuStyles = isMenuOpen ? 'h-screen z-10' : 'h-[0rem]';
   const bodyRef = useRef<HTMLBodyElement | null>(null);
+  const pathname = usePathname();
 
   function handleOpenMenu() {
     setIsMenuOpen(!isMenuOpen);
@@ -27,26 +30,23 @@ export function NavbarMobile(props: NavbarMobileProps) {
   }, [isMenuOpen]);
 
   return (
-    <nav className="sticky top-0 flex justify-between bg-primary px-8 py-4 md:hidden">
-      <button type="button" onClick={handleOpenMenu}>
+    <header className="sticky top-0 flex items-center justify-between bg-background px-8 py-4 md:hidden">
+      <button name="navigation menu" type="button" onClick={handleOpenMenu}>
         <Menu className="cursor-pointer md:hidden" />
       </button>
-      <div
-        className={cn(
-          'absolute top-16 flex w-full flex-col overflow-hidden bg-primary transition-all duration-150 ease-in',
-          menuStyles,
-        )}
-      >
-        {navigation.map((item) => (
-          <NavItem {...item} onClick={() => handleOpenMenu()} />
-        ))}
-      </div>
-      <button
-        type="button"
-        className=" rounded-3xl bg-[#48347A] px-4 py-2 font-bold text-white hover:bg-purple-600 hover:duration-200"
-      >
-        Descarga la app
-      </button>
-    </nav>
+      <nav>
+        <ul
+          className={cn(
+            'absolute left-0 top-16 flex w-full flex-col space-y-8 overflow-hidden bg-background px-8 pt-4 transition-all duration-150 ease-in',
+            menuStyles,
+          )}
+        >
+          {navigation.map((item) => (
+            <NavItem {...item} onClick={() => handleOpenMenu()} active={pathname === item.href} />
+          ))}
+        </ul>
+      </nav>
+      <AppDownloadButton />
+    </header>
   );
 }
