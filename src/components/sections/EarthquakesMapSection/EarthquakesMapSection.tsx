ab90c13a07b89bb2785b1cdb059b452737a94c4a
andonirdgz <andoni_rdgz@outlook.com>
2023-12-08 14:00:32 -0600

    fix: :lipstick: fixed colors on earthquakes map and home hero sections

--- src/components/sections/EarthquakesMapSection/EarthquakesMapSection.tsx
+++ src/components/sections/EarthquakesMapSection/EarthquakesMapSection.tsx
@@ -3,7 +3,7 @@ import { EarthquakesMap } from '@/features/earthquakes-map/EarthquakesMap';
 function EarthquakesMapSection() {
   return (
     <section className="container mx-auto my-8 mt-8 text-center text-5xl font-semibold ">
-      <hr className="mx-auto h-px max-w-7xl bg-black" />
+      <hr className="mx-auto h-px max-w-7xl border-foreground" />
       <h2 className="my-8 mb-8">Últimos sismos</h2>
       <EarthquakesMap />
     </section>
