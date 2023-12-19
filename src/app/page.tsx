eb8843df20d8214792ad320988ca0830c137fd65
DerekRdgz <derek_rdgz@outlook.com>
2023-12-18 20:41:29 -0600

    feat: :sparkles:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { HomeHeroSection, EarthquakesMapSection } from '@/components/sections';
+import RitcherCards from '@/components/sections/Cards/RitcherCard';
 
 function HomePage() {
   return (
     <>
       <HomeHeroSection />
       <EarthquakesMapSection />
+      <RitcherCards />
     </>
   );
 }
