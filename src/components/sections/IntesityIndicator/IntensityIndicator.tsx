915c83d0c926d922f4859bf510e43943f0a8b548
limberg05 <128012393+limberg05@users.noreply.github.com>
2024-01-02 22:25:44 -0600

    feat: :sparkles: earthquake table, and intensity indicator
    intensity does not show in the intensity indicator

---
+++ src/components/sections/IntesityIndicator/IntensityIndicator.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import React from 'react';
+import cn from 'classnames';
+
+const LIGHT: number = 2;
+const MODERATE: number = 4;
+const STRONG: number = 6;
+
+interface IntensityIndicatorProps {
+  intensity: number;
+}
+
+const getIntensityColor = (intensity: number): string => {
+  if (intensity < LIGHT) return 'border-ritcher-scales-green';
+  if (intensity < MODERATE) return 'border-ritcher-scales-yellow';
+  if (intensity < STRONG) return 'border-ritcher-scales-orange';
+  return 'border-ritcher-scales-red';
+};
+
+export function IntensityIndicator({ intensity }: IntensityIndicatorProps) {
+  return (
+    <div
+      role="presentation"
+      className={cn(
+        'flex h-11 w-11 items-center justify-center rounded-full border border-red-500 p-px text-center',
+        getIntensityColor(intensity),
+      )}
+    >
+      <div
+        role="presentation"
+        className={cn(
+          'flex h-full w-full items-center justify-center rounded-full border border-red-500 p-px',
+          getIntensityColor(intensity),
+        )}
+      >
+        <p
+          className={cn(
+            'flex h-full w-full items-center justify-center rounded-full border border-red-500',
+            getIntensityColor(intensity),
+          )}
+        >
+          {intensity?.toFixed(1)}
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,3 +1,7 @@
 export { default as EarthquakesMapSection } from './EarthquakesMapSection/EarthquakesMapSection';
+export * from './EarthquakesTableSection/Columns';
+export * from './EarthquakesTableSection/DataTable';
 export { default as HomeHeroSection } from './HomeHeroSection/HomeHeroSection';
+export * from './IntesityIndicator/IntensityIndicator';
 export * from './legal-notice-section/LegalNoticeSection';
+export * from './legal-notice-section/content';
