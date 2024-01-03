39f6c61f296bada8e9d54cf37ce775f7a4d6c6e1
limberg05 <128012393+limberg05@users.noreply.github.com>
2024-01-02 22:26:40 -0600

    feat: :sparkles: added earthquake table function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
-import { HomeHeroSection, EarthquakesMapSection } from '@/components/sections';
+import { columns } from '@/components/sections/EarthquakesTableSection/Columns';
+import { DataTable } from '@/components/sections/EarthquakesTableSection/DataTable';
+import { EarthquakesMapSection, HomeHeroSection } from '@/components/sections';
 
-function HomePage() {
+export default async function HomePage() {
+  const data = [
+    {
+      intensidad: 5.5,
+      localidad: 'Chiapas',
+      estado: 'pending',
+      fecha: '11/02/2025',
+      hora: '10:50',
+    },
+    {
+      intensidad: 2.3,
+      localidad: 'Acapulco',
+      estado: 'pending',
+      fecha: '12/02/2025',
+      hora: '10:50',
+    },
+  ];
+
   return (
     <>
       <HomeHeroSection />
       <EarthquakesMapSection />
+      <DataTable columns={columns} data={data} />
     </>
   );
 }
-
-export default HomePage;
