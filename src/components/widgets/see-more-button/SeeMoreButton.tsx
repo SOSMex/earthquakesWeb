fab395cb942984c8ed06e4f9d58dfc695d8b6952
DerekRdgz <derek_rdgz@outlook.com>
2024-01-02 20:01:34 -0600

    refactor: :recycle: Review changes

--- src/components/widgets/see-more-button/SeeMoreButton.tsx
+++ src/components/widgets/see-more-button/SeeMoreButton.tsx
@@ -4,14 +4,12 @@ import Link from 'next/link';
 
 export function SeeMoreButton() {
   return (
-    <div className="flex justify-center">
-      <Link
-        href="/earthquakes"
-        rel="noopener noreferrer"
-        className="whitespace-nowrap rounded-3xl bg-brand px-4 py-2 font-medium text-background hover:bg-brand/80 hover:duration-200 "
-      >
-        Listado de sismos
-      </Link>
-    </div>
+    <Link
+      href="/earthquakes"
+      rel="noopener noreferrer"
+      className="mx-auto flex w-fit justify-center whitespace-nowrap rounded-3xl bg-brand px-4 py-2 font-medium text-background hover:bg-brand/80 hover:duration-200"
+    >
+      Listado de sismos
+    </Link>
   );
 }
