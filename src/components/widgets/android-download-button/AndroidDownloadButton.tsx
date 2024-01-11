4255279f13caccb7d4e1f1ef08654f20305bb07c
DerekRdgz <derek_rdgz@outlook.com>
2024-01-10 22:31:40 -0600

    feat: :sparkles: Android and IOS download buttons added

---
+++ src/components/widgets/android-download-button/AndroidDownloadButton.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+export function AndroidDownloadButton() {
+  return (
+    <a
+      href={process.env.NEXT_PUBLIC_APP_LINK as string}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="whitespace-nowrap rounded-3xl bg-[#9985CB] px-4 py-2 font-bold text-background text-black hover:bg-[#9985CB]/80 hover:duration-200 dark:bg-[#48347A] dark:text-white"
+    >
+      Android
+    </a>
+  );
+}
