2c4aa92f42c81f20c5381ce97132c2b42991c42d
andonirdgz <andoni_rdgz@outlook.com>
2024-01-11 17:56:16 -0600

    refactor: :recycle: adjusted style to match the design file

--- src/components/sections/SeeMoreSection/SeeMoreSection.tsx
+++ src/components/sections/SeeMoreSection/SeeMoreSection.tsx
@@ -2,13 +2,9 @@ import { SeeMoreButton } from '@/components/widgets/see-more-button/SeeMoreButto
 
 export function SeeMoreSection() {
   return (
-    <div className="bg-brand-background w-full">
-      <section
-        className="container
-        mx-auto my-8 p-8
-        "
-      >
-        <h2 className="mb-6 text-center text-2xl font-semibold text-black dark:text-white">
+    <div className="mt-12 w-full bg-brand-background">
+      <section className="container mx-auto space-y-7 py-10">
+        <h2 className="text-center text-2xl font-semibold text-primary">
           ¿Quieres enterarte de más sismos?
         </h2>
         <SeeMoreButton />
