edec3f912fe16533b16645b232b1709e5fa87526
DerekRdgz <derek_rdgz@outlook.com>
2024-01-23 13:07:34 -0600

    feat: :construction: EarthquakesMap Clusters (Broken)

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,5 +1,5 @@
 export * from './SeeMoreSection/SeeMoreSection';
-export { default as EarthquakesMapSection } from './earthquakes-map-section/EarthquakesMapSection';
+export * from './earthquakes-map-section/EarthquakesMapSection';
 export { default as HomeHeroSection } from './home-hero-section/HomeHeroSection';
 export * from './legal-notice-section/LegalNoticeSection';
 export * from './legal-notice-section/content';
--- src/features/earthquakes-map/EarthquakesMap.tsx
+++ src/features/earthquakes-map/EarthquakesMap.tsx
@@ -1,19 +1,28 @@
 'use client';
 
-// import { useState } from "react";
-// import { APIProvider, Map } from '@vis.gl/react-google-maps';
+import { useState } from 'react';
+import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
+
+const earthquakeData = [
+  { id: 1, lat: 19.24647, lng: -99.10135 },
+  { id: 2, lat: 18.134478, lng: -94.458986 },
+  { id: 3, lat: 16.849418, lng: -99.908916 },
+];
 
 export function EarthquakesMap() {
-  // const position = { lat: 53.54, lng: 10 };
-
-  console.log(process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY);
+  const position = { lat: 19.432608, lng: -99.133209 };
+  const [zoom, setZoom] = useState(5);
+  const [markers, setMarkers] = useState(earthquakeData);
 
   return (
-    // <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string}>
-    //   <div style={{ height: '100vh' }}>
-    //     <Map zoom={9} center={position} />
-    //   </div>
-    // </APIProvider>
-    <>a</>
+    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string}>
+      <div style={{ height: '350px', width: '100%' }}>
+        <Map zoom={5} center={position}>
+          {markers.map((marker) => (
+            <Marker key={marker.id} position={{ lat: marker.lat, lng: marker.lng }} />
+          ))}
+        </Map>
+      </div>
+    </APIProvider>
   );
 }
