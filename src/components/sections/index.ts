a6bdf79f41e70cbed08d41e90c8f4516cc0a9d6d
andonirdgz <andoni_rdgz@outlook.com>
2024-01-22 20:40:37 -0600

    fix: :coffin: removed dead code

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -6,4 +6,3 @@ export { default as HomeHeroSection } from './home-hero-section/HomeHeroSection'
 export * from './legal-notice-section/LegalNoticeSection';
 export * from './legal-notice-section/content';
 export * from './ritcher-scale-section/RitcherScaleSection';
-export { default as TablePagination } from './table-pagination/TablePagination';
