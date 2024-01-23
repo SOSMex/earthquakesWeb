3f5cee4611b07aedfd59ec7994fdc9591a9a99c6
andonirdgz <andoni_rdgz@outlook.com>
2024-01-22 20:36:28 -0600

    refactor: :recycle: converted getEarthquakesData to be server-side executed

---
+++ src/services/earthquakes/earthquakes.service.ts
@@ -0,0 +1,18 @@
+'use server';
+
+import { BASE_URL } from '@/config';
+
+export async function getEarthquakesData(limit: number = 10, page: number = 1) {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/api/earthquakes/latest?key=${process.env.SELF_SECRET}&limit=${limit}&page=${page}`,
+    );
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+    return {};
+  }
+}
