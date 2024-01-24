8cf12123eefbe302bdeafd6383caab52d59a1646
andonirdgz <andoni_rdgz@outlook.com>
2024-01-23 19:50:25 -0600

    refactor: :recycle: earthquakes map section now reads the data from the data provider

--- src/components/sections/earthquakes-map-section/EarthquakesMapSection.tsx
+++ src/components/sections/earthquakes-map-section/EarthquakesMapSection.tsx
@@ -1,11 +1,13 @@
 import { EarthquakesMap } from '@/features';
+import { useEarthquakesData } from '@/components/providers';
 
 export function EarthquakesMapSection() {
+  const { earthquakes } = useEarthquakesData();
   return (
     <section className="container mx-auto my-8 mt-8 text-center text-5xl font-semibold ">
       <hr className="mx-auto h-px max-w-7xl border-foreground" />
       <h2 className="my-8 mb-8">Últimos sismos</h2>
-      <EarthquakesMap />
+      <EarthquakesMap earthquakes={earthquakes} />
     </section>
   );
 }
