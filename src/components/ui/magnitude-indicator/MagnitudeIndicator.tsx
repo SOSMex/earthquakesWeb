a50c1e90a459d8a9e7e9bfdb0ef63dde2f84d5ce
andonirdgz <andoni_rdgz@outlook.com>
2024-01-25 14:42:38 -0600

    refactor: :lipstick: applied new magnitude indicator design

--- src/components/ui/magnitude-indicator/MagnitudeIndicator.tsx
+++ src/components/ui/magnitude-indicator/MagnitudeIndicator.tsx
@@ -1,41 +1,32 @@
 import { cn } from '@/utils';
 import { MagnitudeIndicatorProps } from './model';
-
-const LIGHT: number = 3;
-const MODERATE: number = 5;
-const STRONG: number = 7;
-
-const getMagnitudeColor = (magnitude: number): string => {
-  if (magnitude < LIGHT) return 'border-ritcher-scales-green';
-  if (magnitude < MODERATE) return 'border-ritcher-scales-yellow';
-  if (magnitude < STRONG) return 'border-ritcher-scales-orange';
-  return 'border-ritcher-scales-red';
-};
+import { getMagnitudeColors } from './helpers';
 
 export function MagnitudeIndicator(props: MagnitudeIndicatorProps) {
   const { magnitude, className } = props;
-  const color = getMagnitudeColor(magnitude);
+  const { border, background, shadow } = getMagnitudeColors(magnitude);
 
   return (
     <div
       role="presentation"
       className={cn(
-        'flex h-11 w-11 items-center justify-center rounded-full border border-red-500 p-px text-center',
-        color,
+        'flex size-11 items-center justify-center rounded-full border-[0.156rem] border-red-500 p-[0.094rem] text-center drop-shadow-sm',
+        border,
         className,
+        shadow,
       )}
     >
       <div
         role="presentation"
         className={cn(
-          'flex h-full w-full items-center justify-center rounded-full border border-red-500 p-px',
-          color,
+          'flex h-full w-full items-center justify-center rounded-full border-[0.156rem] border-red-500 p-0.5',
+          border,
         )}
       >
         <p
           className={cn(
-            'flex h-full w-full items-center justify-center rounded-full border border-red-500',
-            color,
+            'flex h-full w-full items-center justify-center rounded-full border-red-500 text-xs text-black',
+            background,
           )}
         >
           {magnitude?.toFixed(1)}
