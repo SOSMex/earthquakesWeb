9cee62b3987b42943ca84ed2f3e8b05ca5d64560
limberg05 <128012393+limberg05@users.noreply.github.com>
2024-01-29 12:15:47 -0600

    feat: :sparkles: dded vercel analytics fuction

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/providers/AppThemeProvider/AppThemeProvider';
 import { NavBar } from '@/features';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         >
           <NavBar />
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
