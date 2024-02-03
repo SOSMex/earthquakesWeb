4912a2e82aa5be9ad436e68f54906def69e36abb
limberg05 <hernandezdelriojuan@gmail.com>
2024-02-03 15:31:20 -0600

    feat: :lipstick: fixed ui gub and aligment

--- src/components/sections/DownloadSection/DownloadSection.tsx
+++ src/components/sections/DownloadSection/DownloadSection.tsx
@@ -3,26 +3,26 @@ import Image from 'next/image';
 
 export function DownloadSection() {
   return (
-    <div className="w-full bg-brand px-28 py-12">
+    <div className="w-full bg-brand px-10 py-12 md:px-64">
       <section id="download" className="mx-auto flex w-full max-w-5xl flex-col gap-24 lg:flex-row">
-        <div className="flex flex-col justify-center gap-10 lg:w-1/2">
+        <div className="flex flex-col justify-center gap-10 text-center lg:w-1/2">
           <h2 className="text-4xl font-semibold text-white">¡Descarga la App!</h2>
-          <p className="text-white">
+          <p className=" text-white">
             ¿Sabías que en México ocurren, en promedio, 4 sismos al día? ¡No te preocupes! A partir
             de ahora, ¡estarás siempre informado!
           </p>
-          <div className="flex gap-7">
+          <div className="flex justify-center gap-7">
             <AppDownloadButton
               href={process.env.NEXT_PUBLIC_ANDROID_APP_URL as string}
               label="Android"
               target="_blank"
-              className="hover:bg-brand-bright/80 bg-brand-bright text-brand"
+              className="bg-brand-bright text-brand hover:bg-brand-bright/80"
             />
             <AppDownloadButton
               href={process.env.NEXT_PUBLIC_IOS_APP_URL as string}
               label="Apple Store"
               target="_blank"
-              className="hover:bg-brand-bright/80 bg-brand-bright text-brand"
+              className="bg-brand-bright text-brand hover:bg-brand-bright/80"
             />
           </div>
         </div>
